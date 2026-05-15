b067e35c40c79c16a8fd2c8b8ac21e3f2a9b59c6
Yasmin Vieira <vieiracyasmin@gmail.com>
2026-05-15 08:31:52 -0300

    JavaScript

---
+++ javaScript/javascript.js
@@ -0,0 +1,2 @@
+alert("Alerta em arquivo externo")
+document.querySelector("button").innerHTML="Amanhã vou estudar cedinho";
---
+++ javaScript/variaveis.js
@@ -0,0 +1,21 @@
+let a=10
+console.log(a)
+console.log(typeof(a))
+a="dez"
+console.log(a)
+console.log(typeof(a))
+a=true
+console.log(a)
+console.log(typeof(a))
+a=5;
+console.log(a+ "0")
+console.log("0" + a)
+const pi=3.141516
+//pi = 2
+let contador =0
+contador++
+console.log(contador)
+
+for(contador=0;contador<10;contador++){
+    console.log(contador)
+}
